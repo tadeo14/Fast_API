c519422ff6cbc8023a032bdd96d8bbb71e702d35
tadeo <tadeoacosta14@gmail.com>
2025-01-10 17:36:45 -0300

    funciona el puerto local

--- tutorial-env/main.py
+++ tutorial-env/main.py
@@ -2,3 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/") # es un decorador, utilizamos un metodo http. En este caso la ruta del endpoint es un #
+async def hello_world():
+    return {"message": "Hello, World!"}
